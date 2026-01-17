718da9e88a614f15e59dbb9becbcb70e56462df7
Dipak <dipakpatil200412@gmail.com>
2026-01-17 14:03:52 +0530

    the next commit

--- UseEffect/src/App.jsx
+++ UseEffect/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [count, setCount]=useState(0)
+  const [total, setTotal]=useState(0)
+// WITHOUT DEPENDENCY ARRAY IT RUNS ON  EVERY RENDER RENDER
+//  useEffect(()=>{
+//   alert("alert on each render")
+//  })
+
+
+// WITH DEPENDENCY ARRAY IT RUNS ONLY FIRST RENDER
+// useEffect(()=>{
+//  alert("i will run on only first render");
+// },[])
+
+
+//VARIATION NUMBER 4 
+// useEffect(()=>{
+//  alert("i will run on count is updated");
+// },[count])
+
+//VARIATION NUMBER 5
+
+// useEffect(()=>{
+//  alert("i will run on count and total will updatedupdated");
+// },[count,total])
+//   function handleClick(){
+//     setCount(count+1);
+    
+//   }
+
+//VARIATION NUMBER 5
+
+ useEffect(()=>{
+alert("count is updated")
+return()=>{
+  alert("count is unmounted from ui")
+}
+ },[count])
+
+  function handleClickTotal(){
+    setTotal(total+1);
+  }
+    function handleClick(){
+    setCount(count+1);
+    }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <button onClick={handleClick}> click me</button>
+  <br />
+  count is: {count}
+  <br />
+   <button onClick={handleClickTotal}> click me</button>
+  <br />
+  update total: {total}
     </>
   )
 }
+
 
 export default App
